62fbdbd8fa560647029befbf65d47cf90532b043
CMDR-JohnAlex <94056103+CMDR-JohnAlex@users.noreply.github.com>
2022-07-07 14:57:47 -0400

    Added controls for the fill color

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,42 @@ int main() {
 	window.clearColor = { 0.5f, 0.0f, 0.4f, 1.0f };
 
 	while (!window.shouldClose) {
+		if (glfwGetKey(window.window, GLFW_KEY_ESCAPE)) {
+			window.shouldClose = true;
+		}
+
+		if (glfwGetKey(window.window, GLFW_KEY_RIGHT)) {
+			if (window.clearColor[1] < 1.0f) {
+				window.clearColor[1] += 0.01f;
+			}
+			else {
+				window.clearColor[1] = 1.0f;
+			}
+		} if (glfwGetKey(window.window, GLFW_KEY_LEFT)) {
+			if (window.clearColor[1] > 0.0f) {
+				window.clearColor[1] -= 0.01f;
+			}
+			else {
+				window.clearColor[1] = 0.0f;
+			}
+		}
+
+		if (glfwGetKey(window.window, GLFW_KEY_UP)) {
+			if (window.clearColor[0] < 1.0f) {
+				window.clearColor[0] += 0.01f;
+			}
+			else {
+				window.clearColor[0] = 1.0f;
+			}
+		} if (glfwGetKey(window.window, GLFW_KEY_DOWN)) {
+			if (window.clearColor[0] > 0.0f) {
+				window.clearColor[0] -= 0.01f;
+			}
+			else {
+				window.clearColor[0] = 0.0f;
+			}
+		}
+
 		window.clear();
 		// graphics.render();
 		window.update();
